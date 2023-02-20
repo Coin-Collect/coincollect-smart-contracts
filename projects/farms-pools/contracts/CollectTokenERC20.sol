0972fbc5c1c9a67d59e06cc07a50b8ebe86e262d
r <100163255+r0b0roach@users.noreply.github.com>
2023-02-20 13:57:30 +0300

    Create CollectTokenERC20.sol

---
+++ projects/farms-pools/contracts/CollectTokenERC20.sol
@@ -0,0 +1,43 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.9;
+
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+import "@openzeppelin/contracts/token/ERC20/extensions/draft-ERC20Permit.sol";
+import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Votes.sol";
+
+contract CollectToken is ERC20, Ownable, ERC20Permit, ERC20Votes {
+    uint256 public constant maxSupply = 500000000 * 1e18; 
+    
+    constructor() ERC20("Collect Token", "Collect") ERC20Permit("Collect Token") {
+        _mint(msg.sender, 50 * 10 ** decimals());
+    }
+
+    function mint(address to, uint256 amount) public onlyOwner {
+        _mint(to, amount);
+    }
+
+    // The following functions are overrides required by Solidity.
+
+    function _afterTokenTransfer(address from, address to, uint256 amount)
+        internal
+        override(ERC20, ERC20Votes)
+    {
+        super._afterTokenTransfer(from, to, amount);
+    }
+
+    function _mint(address to, uint256 amount)
+        internal
+        override(ERC20, ERC20Votes)
+    {
+        require(totalSupply() + amount <= maxSupply, "reach max supply");
+        super._mint(to, amount);
+    }
+
+    function _burn(address account, uint256 amount)
+        internal
+        override(ERC20, ERC20Votes)
+    {
+        super._burn(account, amount);
+    }
+}

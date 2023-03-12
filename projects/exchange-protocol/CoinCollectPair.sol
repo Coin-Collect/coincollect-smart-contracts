67be5a584a320139b6b1360709a61ea3b46462e3
r <100163255+r0b0roach@users.noreply.github.com>
2023-03-12 16:28:59 +0300

    Swap Fee Set As 0.25%

--- projects/exchange-protocol/CoinCollectPair.sol
+++ projects/exchange-protocol/CoinCollectPair.sol
@@ -180,9 +180,9 @@ contract CoinCollectPair is CoinCollectERC20 {
         uint amount1In = balance1 > _reserve1 - amount1Out ? balance1 - (_reserve1 - amount1Out) : 0;
         require(amount0In > 0 || amount1In > 0, 'CoinCollect: INSUFFICIENT_INPUT_AMOUNT');
         { // scope for reserve{0,1}Adjusted, avoids stack too deep errors
-        uint balance0Adjusted = balance0.mul(1000).sub(amount0In.mul(2));
-        uint balance1Adjusted = balance1.mul(1000).sub(amount1In.mul(2));
-        require(balance0Adjusted.mul(balance1Adjusted) >= uint(_reserve0).mul(_reserve1).mul(1000**2), 'CoinCollect: K');
+        uint balance0Adjusted = (balance0.mul(10000).sub(amount0In.mul(25)));
+        uint balance1Adjusted = (balance1.mul(10000).sub(amount1In.mul(25)));
+        require(balance0Adjusted.mul(balance1Adjusted) >= uint(_reserve0).mul(_reserve1).mul(10000**2), 'CoinCollect: K');
         }
 
         _update(balance0, balance1, _reserve0, _reserve1);

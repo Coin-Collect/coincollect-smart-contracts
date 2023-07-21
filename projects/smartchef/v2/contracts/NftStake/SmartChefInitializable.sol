c03b4c6d5f0c75933926f38ba99e52b85e254291
r <100163255+r0b0roach@users.noreply.github.com>
2023-07-21 14:08:09 +0300

    walletOfOwner removed

--- projects/smartchef/v2/contracts/NftStake/SmartChefInitializable.sol
+++ projects/smartchef/v2/contracts/NftStake/SmartChefInitializable.sol
@@ -597,32 +597,5 @@ contract SmartChefInitializable is Ownable, ReentrancyGuard {
 
         return true;
     }
-    /*
-    function walletOfOwner(address _owner) external view returns(uint256[] memory) {
-        uint256 tokenCount = stakedToken.balanceOf(_owner);
-        if (tokenCount == 0) {
-            return new uint256[](0);
-        } else {
-            uint256[] memory ownedTokenIds = new uint256[](tokenCount);
-            uint256 index;
-            uint256 loopThrough = IERC721Enumerable(address(stakedToken)).totalSupply();
-
-            for (uint256 tokenId = 0; tokenId <= loopThrough; tokenId++) {
-                if (index == tokenCount) break;
-
-                try stakedToken.ownerOf(tokenId) returns (address result) {
-                    if (result == _owner) {
-                        ownedTokenIds[index] = tokenId;
-                        index++;
-                    }
-                } catch {
-                       loopThrough++;
-                }
-                
-            }
-
-            return ownedTokenIds;
-        }
-    }
-    */
+    
 }
